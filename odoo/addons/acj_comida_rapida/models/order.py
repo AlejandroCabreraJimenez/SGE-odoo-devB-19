12ee5720cc8f7641a897196b1141f078a0d9e045
Alejandro <cabrerajimenezalex79@gmail.com>
2026-04-13 03:27:03 +0200

    CORECCION DE ERRORES 1

--- odoo/addons/acj_comida_rapida/models/order.py
+++ odoo/addons/acj_comida_rapida/models/order.py
@@ -7,16 +7,19 @@ class AcjComidaRapidaOrder(models.Model):
     _name = 'acj_comida_rapida.order'
     _description = 'Pedido'
 
-    name = fields.Char(string='Número de pedido', required=True, default=lambda self: self._generate_order_number())
+    name = fields.Char(string='Número de pedido', required=True, default='ORD001')
     date = fields.Date(string='Fecha', default=fields.Date.today)
     customer_id = fields.Many2one('res.partner', string='Cliente')
     order_line_ids = fields.One2many('acj_comida_rapida.order_line', 'order_id', string='Líneas de pedido')
     total = fields.Float(string='Total', compute='_compute_total', store=True)
 
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'ORD001') == 'ORD001':
+            vals['name'] = self.env['ir.sequence'].next_by_code('acj_comida_rapida.order') or 'ORD001'
+        return super().create(vals)
+
     @api.depends('order_line_ids.price_subtotal')
     def _compute_total(self):
         for order in self:
             order.total = sum(line.price_subtotal for line in order.order_line_ids)
-
-    def _generate_order_number(self):
-        return self.env['ir.sequence'].next_by_code('acj_comida_rapida.order') or 'ORD001'
